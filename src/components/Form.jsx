0b5f1318cdf8d127b689efcbc51e6f00bd486799
mvrz <msv930@gmail.com>
2025-11-15 11:48:39 -0600

    Fixed state updating to local storage

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,27 @@
 import { useLocalStorage } from "../hooks/useLocalStorage";
 function Form() {
+    const [ name, changeName ] = useLocalStorage("name", "")
 
+    const [ serviceNumber, changeNumber ] = useLocalStorage("serviceNumber", "")
+
+    function handleSubmit(e) {
+      e.preventDefault()
+    }
+
+    function handleNameChange(e) {
+      changeName(e.target.value)
+    }
+
+    function handleNumberChange(e) {
+      changeNumber(e.target.value)
+    }
     return (
       <>
-        <form style={{ display: "flex", flexDirection: "column" }}>
+        <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit}>
             <label htmlFor="name">Name:</label>
-            <input type="text" data-testid={"name"} />
+            <input type="text" data-testid={"name"} value={name} onChange={handleNameChange} />
             <label htmlFor="service">Service Number:</label>
-            <input type="text" data-testid={"service"} />
+            <input type="text" data-testid={"service"} value={serviceNumber} onChange={handleNumberChange} />
 
         </form>
         <h4>{name ? `Welcome, ${name}!` : "Enter your name"}</h4>
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,14 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+
+    const [storedData, changeData] = useState(() => {
+        return localStorage.getItem(key) ? localStorage.getItem(key) : initialValue
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, storedData)
+    }, [key, storedData])
+
+    return [ storedData, changeData ]
 }
